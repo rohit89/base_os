39ba866b7afcf492ca2a088c10966bc5416ff4f8
Rohit Mathulla <rohit_mathulla@yahoo.com>
2012-12-31 12:39:45 +0530

    print message on boot

--- boot.asm
+++ boot.asm
@@ -1,3 +1,19 @@
-hlt
-times 509 db 0
+org 07C00h
+
+mov si, msg
+
+print:
+  lodsb
+  cmp al, 0
+  je halt
+
+  mov ah, 0Eh
+  int 10h
+  jmp print
+
+halt:
+  hlt
+
+msg db "Booting...hello !!"
+times 510 - ($ - $$) db 0
 dw 0xaa55
