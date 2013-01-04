49d887a7c635f52ff56c6f729f9ef9b9823061b0
Rohit Mathulla <rohit_mathulla@yahoo.com>
2013-01-04 10:38:03 +0530

    add comment to source

--- boot.asm
+++ boot.asm
@@ -1,19 +1,21 @@
-org 07C00h
+org 07C00h                           ; code will be loaded at this address and nasm can calculate
+                                     ; offsets with it
 
-mov si, msg
+mov si, msg                          ; move message to source index  
 
 print:
-  lodsb
+  lodsb                              ; load value in al register and increment
+                                     ; si
   cmp al, 0
   je halt
 
-  mov ah, 0Eh
-  int 10h
+  mov ah, 0Eh                        ; value required for int 10h
+  int 10h                            ; video interrupt
   jmp print
 
 halt:
   hlt
 
-msg db "Booting...hello !!"
-times 510 - ($ - $$) db 0
-dw 0xaa55
+msg db "Booting...hello !!", 0
+times 510 - ($ - $$) db 0            ; fill remaining space with 0
+dw 0xaa55                            ; boot signature
